0361214dea8005492a7f69832185692486f8529b
mayur <mayurlaishetti22@gmail.com>
2019-01-18 11:44:49 +0530

    ok

---
+++ src/com/focus/beans/CLActionBean.java
@@ -0,0 +1,30 @@
+package com.focus.beans;
+
+import java.io.Serializable;
+
+/**
+ * Created by Dipak on 1/7/2019.
+ */
+public class CLActionBean
+{
+    String label;
+    String action;
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+
+
+}
